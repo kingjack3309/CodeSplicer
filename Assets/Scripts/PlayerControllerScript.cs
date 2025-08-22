8b8c284408cd758b2d9984ed868cd0cc346aa675
kingjack3309 <jacobbeston@student.doniphanr1.com>
2025-08-22 15:45:07 -0500

    did stuff

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -5,26 +5,43 @@ using UnityEngine.UIElements;
 
 public class PlayerControllerScript : MonoBehaviour
 {
+    private float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    Rigidbody2D rb;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-        { 
-            
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            
-        }
+        horizontal = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity =new Vector2(speed * horizontal, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal <0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
