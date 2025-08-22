979389f29bff93e563de3a16ca5385a847819223
kingjack3309 <jacobbeston@student.doniphanr1.com>
2025-08-22 15:14:01 -0500

    fixed player sprites and added some scripts

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        { 
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+
+    }
+}
