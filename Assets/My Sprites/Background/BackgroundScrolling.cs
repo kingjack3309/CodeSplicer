a4fe024ad8842f4dab27910ed22b851b539f0235
kingjack3309 <jacobbeston@student.doniphanr1.com>
2025-10-16 11:38:48 -0500

    weeee i have a parallax background

--- Assets/My Sprites/Background/BackgroundScrolling.cs
+++ Assets/My Sprites/Background/BackgroundScrolling.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
 [System.Serializable]
-
 public class BackgroundElement
 {
     public SpriteRenderer backgroundSprite;
 
-    [Range(0, 1)] public float scrollSpeed;
+    [Range(0, 1)] public float xScrollSpeed;
+    [Range(0, 1.5f)] public float yScrollSpeed;
 
     [HideInInspector] public Material spriteMaterial;
 }
@@ -19,7 +19,7 @@ public class BackgroundScrolling : MonoBehaviour
 
     private void Start()
     {
-        foreach(BackgroundElement element in backgroundElements)
+        foreach (BackgroundElement element in backgroundElements)
         {
             element.spriteMaterial = element.backgroundSprite.material;
         }
@@ -27,9 +27,9 @@ public class BackgroundScrolling : MonoBehaviour
 
     private void Update()
     {
-        foreach(BackgroundElement element in backgroundElements)
+        foreach (BackgroundElement element in backgroundElements)    //X speed                                                       //Y speed
         {
-            element.spriteMaterial.mainTextureOffset = new Vector2(transform.position.x *)
+            element.spriteMaterial.mainTextureOffset = new Vector2(transform.position.x * element.xScrollSpeed * SCROLL_MULTIPLIER, transform.position.y * element.yScrollSpeed * SCROLL_MULTIPLIER);
         }
     }
 }
