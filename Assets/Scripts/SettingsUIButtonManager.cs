278446cddb264e5bd3208cef8ba59151218c446d
kingjack3309 <jacobbeston@student.doniphanr1.com>
2026-01-06 11:34:20 -0600

    we have usable settings
    you can now update your volume mid playthrough instead of having one set volume when you first hit play.
    
    furthermore i am developing a button to toggle particles which should make it easier to run the game.

--- Assets/Scripts/SettingsUIButtonManager.cs
+++ Assets/Scripts/SettingsUIButtonManager.cs
@@ -6,9 +6,63 @@ public class SettingsUIButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
 
+    [SerializeField] GameObject audioMenu;
+
+    [SerializeField] GameObject videoMenu;
+
+    [HideInInspector]
+    public List<ParticleSystem> uiParticles = new List<ParticleSystem>();
+    [HideInInspector]
+    public List<ParticleSystem> dynamicParticles = new List<ParticleSystem>();
+
+    private GameObject openMenu;
+
     public void ExitSettingsMenu()
     {
         pauseMenu.SetActive(true);
         GameObject.Find("SettingsUI").SetActive(false);
     }
+
+    public void ToggleAudioMenu()
+    {
+        if (openMenu != null)
+        {
+            openMenu.SetActive(false); //Closes the open menu if there is any
+        }
+
+        if (openMenu != audioMenu)
+        {
+            openMenu = audioMenu; // open new menu
+            audioMenu.SetActive(true);
+        }
+        else if (openMenu == audioMenu) 
+        {
+            openMenu = null;
+        }
+        
+    }
+
+    public void ToggleVideoMenu()
+    {
+        if (openMenu != null)
+        {
+            openMenu.SetActive(false); //Closes the open menu if there is any
+        }
+
+        if (openMenu != videoMenu)
+        {
+            openMenu = videoMenu; // open new menu
+            videoMenu.SetActive(true);
+        }
+        else if (openMenu == videoMenu)
+        {
+            openMenu = null;
+        }
+    }
+
+    public void ToggleUIParticles()
+    {
+
+    }
+
 }
