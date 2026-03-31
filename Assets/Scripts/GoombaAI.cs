b0c3831230b8804fab123659f65be1396def8cdb
kingjack3309 <131702239+kingjack3309@users.noreply.github.com>
2026-03-31 14:01:28 -0500

    Fahhhhhhhhhhhhhhhhhhh

--- Assets/Scripts/GoombaAI.cs
+++ Assets/Scripts/GoombaAI.cs
@@ -7,6 +7,8 @@ public class GoombaAI : MonoBehaviour
     Vector2 rayForwardDirection;
     Vector2 rayDownDirection = Vector2.down;
 
+    float rayLength = 4;
+
     string enemyFacing = "right";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,15 +29,25 @@ public class GoombaAI : MonoBehaviour
             rayForwardDirection = Vector2.left;
         }
 
-        RaycastHit2D floorDetector = Physics2D.Raycast(rayOrigin, rayDownDirection);
+        RaycastHit2D floorDetector = Physics2D.Raycast(rayOrigin, rayDownDirection, rayLength, LayerMask.GetMask("ground"));
+        RaycastHit2D wallDetector = Physics2D.Raycast(rayOrigin, rayForwardDirection, rayLength, LayerMask.GetMask("ground"));
 
         if (floorDetector.collider == null && enemyFacing == "right")
         {
             enemyFacing = "left";
+            Debug.Log("Facing left");
         }
         else if (floorDetector.collider == null && enemyFacing == "left")
         {
             enemyFacing = "right";
+            Debug.Log("Facing right");
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(rayOrigin, rayForwardDirection);
+        Gizmos.DrawRay(rayOrigin, rayDownDirection);
     }
 }
